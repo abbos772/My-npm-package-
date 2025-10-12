0e853f2d7eaa97d1e86af613ae55825c81c45a69
Abbos <kadirovabbos407@gmail.com>
2025-10-13 03:27:37 +0500

    chore: update build config and package.json

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,19 +1,22 @@
-import { defineConfig } from 'tsup';
+import { defineConfig } from 'tsup'
 
 export default defineConfig({
   entry: ['src/index.ts'],
   format: ['cjs', 'esm'],
-  dts: true,
+  dts: {
+    resolve: true, 
+  },
   outDir: 'dist',
   clean: true,
   sourcemap: true,
   splitting: false,
   treeshake: true,
   external: ['react', 'react-dom'],
+  injectStyle: true, 
   esbuildOptions(options) {
     options.loader = {
       ...options.loader,
       '.css': 'local-css',
-    };
+    }
   },
-});
+})
